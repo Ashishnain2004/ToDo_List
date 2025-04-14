dd83be22335618153ee03ee9248707defbe0e2c6
Ayushr24 <ayushroy2408@gmail.com>
2025-04-15 02:39:39 +0530

    Add: function loadtasts added in App.js for issue #3

--- src/App.js
+++ src/App.js
@@ -67,6 +67,16 @@ export default function App() {
 		saveTasks([...clonedTasks]);
 	}
 
+	function loadTasks() {
+		let loadedTasks = localStorage.getItem('tasks');
+
+		let tasks = JSON.parse(loadedTasks);
+
+		if (tasks) {
+			setTasks(tasks);
+		}
+	}
+
 	function saveTasks(tasks) {
 		localStorage.setItem('tasks', JSON.stringify(tasks));
 	}
